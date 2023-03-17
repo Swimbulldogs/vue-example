0d081e4b8043f0666773e236f3ee9e4df39e593b
Isha Krishnamurthy <isha@Ishas-MacBook-Pro-M1.local>
2023-03-17 11:48:02 -0400

    code

---
+++ src/components/Cart.vue
@@ -0,0 +1,105 @@
+
+<script>
+import NavBar from './NavBar.vue'
+import CartItem from './CartItem.vue'
+
+// Should inherit from app. 
+export default {
+    data() {
+        return {
+            hotdog: this.hotdog,
+            quantity:0
+        }
+        
+    },
+    props: {
+        hotdog: String
+    },
+    components: {NavBar, CartItem},
+
+}
+</script>
+
+<template>
+<header><center> THIS IS THE CART </center></header>
+{{ quantity }}
+<main>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+  <img src="https://images.squarespace-cdn.com/content/v1/5dd8630d09ab5908e35b35a0/1574462664001-48S1VO61L9LNWNJ910CV/img-HotDogStock-1080x675.png" width="200" height="200" />
+  <p>Product: Basic Hotdog</p>
+  <p> This is the most basic hotdog</p>
+  <p> Price: $3.00 </p>
+    <button @click="() => quantity++">: add basic hotdog to cart</button>
+    <button @click="() => quantity--">: delete basic hotdog from cart</button>
+  </CartItem>
+  <br>
+  <br>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+  <img src= "https://previews.agefotostock.com/previewimage/medibigoff/675a51f9f102310bc61c0ec4ec9e40b6/sfd-12991405.jpg" width="200" height="200" />
+  <p>Product: Relish Hotdog</p>
+  <p> This is a hotdog with relish</p>
+  <p> Price: $6.00</p>
+    <button @click="() => quantity++">: add relish hotdog to cart</button>
+    <button @click="() => quantity--">: delete relish hotdog from cart</button>
+  </CartItem>
+  <br>
+  <br>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+      <img src="https://complex-res.cloudinary.com/image/upload/rhcvsg8mhkqdichmwew9.jpg" width="200" height="200" />
+      <p>Product: Hotdog with whipped cream</p>
+      <p> This hotdog has whipped cream</p>
+      <p> Price: $5.00</p>
+    <button @click="() => quantity++">: add whipped cream hotdog to cart</button>
+    <button @click="() => quantity--">: delete whipped cream hotdog from cart</button>
+
+  </CartItem>
+  <br>
+  <br>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+      <img src="https://flyingcdn-cf8480cb.b-cdn.net//wp-content/uploads/2022/01/Chili-Dogs-on-a-Platter.jpg?width=800" width = "200" height = "200" />
+      <p>Product: Hotdog with meat</p>
+      <p> This hotdog has meat</p>
+      <p> Price: $7.00</p>
+      <p> {{ num }}</p>
+    <button @click="() => quantity++">: add meat hotdog to cart</button>
+    <button @click="() => quantity--">: delete meat hotdog from cart</button>
+  </CartItem>
+  <br>
+  <br>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+      <img src="https://www.seriouseats.com/thmb/s6XumFs3Fe4Otd8NejuSXzIVMKU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2012__05__20120529-food-lab-cooking-sausage-hot-dog-19-e55675e3468b4189a38392ece9d77d4d.jpg" width = "200" height = "200" />
+      <p>Product: Hotdog with sauerkraut</p>
+      <p> This hotdog has saurkraut</p>
+      <p> Price: $7.00</p>
+      <p> {{ num }}</p>
+    <button @click="() => quantity++">: add saurkraut hotdog to cart</button>
+    <button @click="() => quantity--">: delete saurkraut hotdog from cart</button>
+  </CartItem>
+  <br>
+  <br>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+      <img src="https://assets3.thrillist.com/v1/image/2810448/1584x1056/crop;jpeg_quality=60;progressive.jpg" width = "200" height = "200" />
+      <p>Product: Hotdog with ketchup</p>
+      <p> This hotdog has ketchup</p>
+      <p> Price: $2.00</p>
+      <p> {{ num }}</p>
+    <button @click="() => quantity++">: add ketchup hotdog to cart</button>
+    <button @click="() => quantity--">: delete ketchup hotdog from cart</button>
+  </CartItem>
+  <br>
+  <br>
+  <CartItem @add-one = "()=>quantity++" @delete-one = "()=>quantity--">
+      <img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/13/0/GC_hot-dog-with-pickle-relish_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1371592835029.jpeg" width = "200" height = "200" />
+      <p>Product: Hotdog with pickles</p>
+      <p> This hotdog has pickles</p>
+      <p> Price: $3.00</p>
+      <p> {{ num }}</p>
+    <button @click="() => quantity++">: add pickle hotdog to cart</button>
+    <button @click="() => quantity--">: delete pickle hotdog from cart</button>
+  </CartItem>
+</main>
+</template>
+
+<style scoped>
+</style>
+
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,27 +9,27 @@ export default {
                     hotdog: "default"
                 },
                 {
-                    src: "https://www.agefotostock.com/age/en/details-photo/hot-dog-with-relish-onion-ketchup-mustard/SFD-12991405",
+                    src: "https://previews.agefotostock.com/previewimage/medibigoff/675a51f9f102310bc61c0ec4ec9e40b6/sfd-12991405.jpg",
                     hotdog: "relish"
                 },
                 {
-                    src: "https://www.pinterest.com/pin/761038037015424106/",
+                    src: "https://complex-res.cloudinary.com/image/upload/rhcvsg8mhkqdichmwew9.jpg",
                     hotdog: "whipped cream"
                 },
                 {
-                    src: "https://juliassimplysouthern.com/2014/05/julias-southern-hot-dog-chili-1.html",
-                    hotdog: "meat on hotdog"
+                    src: "https://flyingcdn-cf8480cb.b-cdn.net//wp-content/uploads/2022/01/Chili-Dogs-on-a-Platter.jpg?width=800",
+                    hotdog: "chilli on hotdog"
                 },
                 {
-                    src: "https://www.seriouseats.com/grilled-hot-dogs-with-sauerkraut-recipe",
+                    src: "https://www.seriouseats.com/thmb/s6XumFs3Fe4Otd8NejuSXzIVMKU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2012__05__20120529-food-lab-cooking-sausage-hot-dog-19-e55675e3468b4189a38392ece9d77d4d.jpg",
                     hotdog: "sauerkraut"
                 },
                 {
-                    src: "https://www.thrillist.com/eat/nation/ketchup-on-hot-dogs-best-toppings",
+                    src: "https://assets3.thrillist.com/v1/image/2810448/1584x1056/crop;jpeg_quality=60;progressive.jpg",
                     hotdog: "ketchup"
                 },
                 {
-                    src: "https://www.foodnetwork.com/recipes/bobby-flay/grilled-link-hot-dogs-with-homemade-pickle-relish-recipe-1942988",
+                    src: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/13/0/GC_hot-dog-with-pickle-relish_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1371592835029.jpeg",
                     hotdog: "pickles"
                 }
             ];
